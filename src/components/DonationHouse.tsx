6597883022ae221848d136bf9e866ba4fe558932
Byron Buckley <byron.buckley@ignitionadvice.com>
2024-11-08 17:25:54 +1100

    blue-church looking good

--- src/components/DonationHouse.tsx
+++ src/components/DonationHouse.tsx
@@ -18,16 +18,41 @@ export function DonationHouse({ currentAmount, goalAmount }: DonationHouseProps)
 
       {/* Church SVG */}
       <div className="absolute inset-0 flex items-center justify-center">
-        <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
-          {/* Background fill that changes with progress */}
-          <path
-            d="M128,58.1l-94.8,85.6h23.9v95.7h141.8v-95.7h23.9L128,58.1z"
-            fill="#b8d8df"
-            fillOpacity={progress / 100}
-            style={{
-              transition: 'fill-opacity 1s ease-in-out'
-            }}
-          />
+        <svg width="280" height="280" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <pattern id="gridPattern" width="20" height="20" patternUnits="userSpaceOnUse">
+              <path d="M 20 0 L 0 0 0 20" fill="none" stroke="rgba(0,0,0,0.1)" strokeWidth="1"/>
+            </pattern>
+            <clipPath id="churchShape">
+              <path d="M128,58.1l-94.8,85.6h23.9v95.7h141.8v-95.7h23.9L128,58.1z" />
+            </clipPath>
+          </defs>
+
+          {/* Progress fill with grid */}
+          <g clipPath="url(#churchShape)">
+            <rect
+              x="0"
+              y="256"
+              width="256"
+              height="256"
+              fill="#b8d8df"
+              style={{
+                transform: `translateY(${-(progress)}%)`,
+                transition: 'transform 1s ease-in-out'
+              }}
+            />
+            <rect
+              x="0"
+              y="256"
+              width="256"
+              height="256"
+              fill="url(#gridPattern)"
+              style={{
+                transform: `translateY(${-(progress)}%)`,
+                transition: 'transform 1s ease-in-out'
+              }}
+            />
+          </g>
 
           {/* Door */}
           <path
@@ -39,17 +64,11 @@ export function DonationHouse({ currentAmount, goalAmount }: DonationHouseProps)
             }}
           />
 
-          {/* Small detail */}
-          <path
-            d="M176.2,150.2c-0.4,0-0.9,0-1.3-0.1c-0.4-0.1-0.8-0.2-1.2-0.4c-0.4-0.2-0.8-0.4-1.1-0.6 c-0.4-0.2-0.7-0.5-1-0.8s-0.6-0.6-0.8-1c-0.2-0.4-0.4-0.7-0.6-1.1c-0.3-0.8-0.5-1.6-0.5-2.5c0-0.4,0-0.9,0.1-1.3 c0.1-0.4,0.2-0.8,0.4-1.2c0.2-0.4,0.4-0.8,0.6-1.1c0.2-0.4,0.5-0.7,0.8-1c0.3-0.3,0.6-0.6,1-0.8c0.7-0.5,1.5-0.8,2.4-1 c0.8-0.2,1.7-0.2,2.6,0c0.4,0.1,0.8,0.2,1.2,0.4c0.4,0.2,0.8,0.4,1.1,0.6c0.4,0.2,0.7,0.5,1,0.8s0.6,0.6,0.8,1 c0.2,0.4,0.4,0.7,0.6,1.1c0.3,0.8,0.5,1.6,0.5,2.5c0,0.4,0,0.9-0.1,1.3c-0.1,0.4-0.2,0.8-0.4,1.2c-0.2,0.4-0.4,0.8-0.6,1.1 c-0.2,0.4-0.5,0.7-0.8,1c-0.3,0.3-0.6,0.6-1,0.8c-0.4,0.2-0.7,0.4-1.1,0.6C177.9,150.1,177.1,150.2,176.2,150.2L176.2,150.2z"
-            fill="#040000"
-          />
-
-          {/* Outline */}
+          {/* Church outline */}
           <path
             d="M227.2,138.8l-92.7-83.6V43.9h14.2c3.6,0,6.6-2.9,6.6-6.6c0-3.6-2.9-6.6-6.6-6.6h-14.2V16.6 c0-3.6-2.9-6.6-6.6-6.6c-3.6,0-6.6,2.9-6.6,6.6v14.2h-14.2c-3.6,0-6.6,2.9-6.6,6.6c0,3.6,2.9,6.6,6.6,6.6h14.2v11.3l-92.7,83.6 c-2.7,2.4-2.9,6.6-0.5,9.3c1.2,1.4,3,2.2,4.9,2.2h17.4v89.2c0,3.6,2.9,6.6,6.6,6.6h141.8c3.6,0,6.6-2.9,6.6-6.6v-89.2h17.4 c3.6,0,6.6-2.9,6.6-6.6C229.4,141.8,228.6,140.1,227.2,138.8L227.2,138.8z"
             stroke="#4e665d"
-            strokeWidth="2"
+            strokeWidth="3"
             fill="none"
           />
         </svg>
